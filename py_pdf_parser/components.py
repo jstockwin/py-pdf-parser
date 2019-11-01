94e2557ab8d970f0dc7a190097fefd8fe70d19a2
Jake Stockwin <jstockwin@gmail.com>
2019-11-01 14:40:54 +0000

    Refactor into components and loaders

--- py_pdf_parser/components.py
+++ py_pdf_parser/components.py
@@ -1,9 +1,8 @@
-from typing import Dict, List, IO, Any, Optional, Iterator
+from typing import Dict, List, Optional, Iterator
 
 from collections import namedtuple
 
-from pdfminer import converter, layout, pdfdocument, pdfinterp, pdfpage, pdfparser
-from pdfminer.layout import LTComponent, LTTextContainer, LAParams
+from pdfminer.layout import LTComponent
 
 
 PageInfo = namedtuple("PageInfo", ["width", "height", "start_index", "end_index"])
@@ -94,49 +93,3 @@ class PDFDocument:
             lambda elem: include_ignored or not elem.ignore,
             self.elements[page_info.start_index : page_info.end_index],
         )
-
-
-def load_file(path_to_file: str) -> PDFDocument:
-    with open(path_to_file, "rb") as in_file:
-        return load(in_file, pdf_file_path=path_to_file)
-
-
-def load(pdf_file: IO, pdf_file_path: Optional[str] = None) -> PDFDocument:
-    parser = pdfparser.PDFParser(pdf_file)
-    document = pdfdocument.PDFDocument(parser)
-
-    if not document.is_extractable:
-        raise pdfpage.PDFTextExtractionNotAllowed
-
-    resource_manager = pdfinterp.PDFResourceManager()
-    device = converter.PDFPageAggregator(
-        resource_manager, laparams=LAParams()
-    )  # TODO laparams
-    interpreter = pdfinterp.PDFPageInterpreter(resource_manager, device)
-
-    elements: List[PDFElement] = []
-    page_info: Dict[int, PageInfo] = {}
-    for page in pdfpage.PDFPage.create_pages(document):
-        interpreter.process_page(page)
-        results = device.get_result()
-
-        page_number = results.pageid
-
-        new_elements = [
-            PDFElement(element=element, page_number=results.pageid)
-            for element in results
-            if isinstance(element, LTTextContainer)
-        ]
-
-        page_info[page_number] = PageInfo(
-            width=results.width,
-            height=results.height,
-            start_index=len(elements),
-            end_index=len(elements) + len(new_elements) - 1,
-        )
-
-        elements += new_elements
-
-    return PDFDocument(
-        elements=elements, page_info=page_info, pdf_file_path=pdf_file_path
-    )
