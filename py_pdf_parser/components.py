e43de836980803a10c1116f440a7780ca4fe3ad8
Jake Stockwin <jstockwin@gmail.com>
2019-11-08 13:54:23 +0000

    Move 'page' from element-level to document-level

--- py_pdf_parser/components.py
+++ py_pdf_parser/components.py
@@ -1,26 +1,25 @@
 from typing import Dict, List, Optional, Iterator
 
-from collections import namedtuple
+from collections import namedtuple, OrderedDict
 
 from pdfminer.layout import LTComponent
 
 
-PageInfo = namedtuple("PageInfo", ["width", "height", "start_index", "end_index"])
+Page = namedtuple("Page", ["width", "height", "elements"])
+PageInfo = namedtuple("PageInfo", ["width", "height", "start_element", "end_element"])
 BoundingBox = namedtuple("BoundingBox", ["x0", "x1", "y0", "y1", "width", "height"])
 
 
 class PDFElement:
     original_element: LTComponent
-    page_number: int
     tags: List[str]
     ignore: bool
     bounding_box: BoundingBox
     __fontname: Optional[str] = None
     __fontsize: Optional[int] = None
 
-    def __init__(self, element: LTComponent, page_number: int):
+    def __init__(self, element: LTComponent):
         self.original_element = element
-        self.page_number = page_number
 
         self.tags = []
         self.ignore = False
@@ -73,27 +72,46 @@ class PDFDocument:
     page_info: Mapping between page number and PageInfo namedtuples.
     """
 
-    elements: List[PDFElement]
-    page_info: Dict[int, PageInfo]
+    elements: List[PDFElement] = []
+    page_info: Dict[int, PageInfo] = {}
     number_of_pages: int
     pdf_file_path: Optional[str]
 
-    def __init__(
-        self,
-        elements: List[PDFElement],
-        page_info: Dict[int, PageInfo],
-        pdf_file_path: Optional[str] = None,
-    ):
-        self.elements = elements
-        self.page_info = page_info
+    def __init__(self, pages: Dict[int, Page], pdf_file_path: Optional[str] = None):
+        self.__elements = OrderedDict()
+        for page_number, page in pages.items():
+            self.page_info[page_number] = PageInfo(
+                width=page.width,
+                height=page.height,
+                start_element=page.elements[0],
+                end_element=page.elements[-1],
+            )
+
+            self.elements += page.elements
+
         self.pdf_file_path = pdf_file_path
-        self.number_of_pages = len(page_info)
+        self.number_of_pages = len(pages)
+
+    def elements_between(
+        self,
+        start_element: PDFElement,
+        end_element: PDFElement,
+        include_ignored: bool = False,
+    ) -> Iterator[PDFElement]:
+
+        start_index = self.elements.index(start_element)
+        end_index = self.elements.index(end_element)
+
+        return filter(
+            lambda elem: include_ignored or not elem.ignore,
+            self.elements[start_index:end_index],
+        )
 
     def elements_for_page(
         self, page_number: int, include_ignored: bool = False
     ) -> Iterator[PDFElement]:
+
         page_info = self.page_info[page_number]
-        return filter(
-            lambda elem: include_ignored or not elem.ignore,
-            self.elements[page_info.start_index : page_info.end_index],
+        return self.elements_between(
+            page_info.start_element, page_info.end_element, include_ignored
         )
