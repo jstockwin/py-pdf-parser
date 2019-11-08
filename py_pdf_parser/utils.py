0cb6437095540064966a7c145306d402e96d8b09
Jake Stockwin <jstockwin@gmail.com>
2019-11-08 15:10:03 +0000

    Add tag elements between util

--- py_pdf_parser/utils.py
+++ py_pdf_parser/utils.py
@@ -1,14 +1,26 @@
-from typing import Dict, List
+from typing import List, Dict, TYPE_CHECKING
 
-from .components import PDFElement, PDFDocument
-
-
-def tag_elements_by_font(elements: List[PDFElement], config: Dict[str, str]):
-    for element in elements:
-        new_tag = config.get(element.font)
-        if new_tag:
-            element.add_tag(new_tag)
+if TYPE_CHECKING:
+    from .components import PDFDocument, PDFElement
 
 
-def tag_all_elements_by_font(document: PDFDocument, config: Dict[str, str]):
-    tag_elements_by_font(document.elements, config)
+class Utils:
+    def __init__(self, document: "PDFDocument"):
+        self.document = document
+
+    def tag_elements_by_font(
+        self, elements: List["PDFElement"], config: Dict[str, str]
+    ):
+        for element in elements:
+            new_tag = config.get(element.font)
+            if new_tag:
+                element.add_tag(new_tag)
+
+    def tag_all_elements_by_font(self, config: Dict[str, str]):
+        self.tag_elements_by_font(self.document.elements, config)
+
+    def tag_elements_between(
+        self, start_element: "PDFElement", end_element: "PDFElement", tag: str
+    ):
+        for element in self.document.elements_between(start_element, end_element):
+            element.add_tag(tag)
