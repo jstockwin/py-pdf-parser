65bf6ed868984257b8def3c0463d25796cbb4ca8
Jake Stockwin <jstockwin@gmail.com>
2019-11-20 16:09:46 +0000

    Add proper exceptions, logging, allow visualise without pdf_file_path

---
+++ py_pdf_parser/exceptions.py
@@ -0,0 +1,33 @@
+class PDFParserError(Exception):
+    pass
+
+
+# Components
+class PageNotFoundError(PDFParserError):
+    pass
+
+
+class NoElementsOnPageError(PDFParserError):
+    pass
+
+
+# Filtering
+class NoElementFoundError(PDFParserError):
+    pass
+
+
+class MultipleElementsFoundError(PDFParserError):
+    pass
+
+
+# Tables
+class TableExtractionError(PDFParserError):
+    pass
+
+
+class InvalidTableError(PDFParserError):
+    pass
+
+
+class InvalidTableHeaderError(PDFParserError):
+    pass
