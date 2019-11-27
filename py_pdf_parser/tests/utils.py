9f91e9d22790af035463eb47a9c68e72057cb9df
Jake Stockwin <jstockwin@gmail.com>
2019-11-27 14:37:26 +0000

    Add missing type hints

--- py_pdf_parser/tests/utils.py
+++ py_pdf_parser/tests/utils.py
@@ -7,7 +7,7 @@ if TYPE_CHECKING:
     from py_pdf_parser.common import BoundingBox
 
 
-def create_element(bounding_box: "BoundingBox", index=0, page_number=1):
+def create_element(bounding_box: "BoundingBox", index: int = 0, page_number: int = 1):
     return PDFElement(
         element=LTComponent(
             bbox=[bounding_box.x0, bounding_box.y0, bounding_box.x1, bounding_box.y1]
