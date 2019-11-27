1e8b69f542287717a75ccde0b3c61361ddc37920
Jake Stockwin <jstockwin@gmail.com>
2019-11-27 14:25:49 +0000

    [docs] Fix incorrect docstrings

--- py_pdf_parser/common.py
+++ py_pdf_parser/common.py
@@ -1,19 +1,19 @@
 class BoundingBox:
     """
     A rectangle, stored using the coordinates (x0, y0) of the bottom left corner, and
-    the coordinates (x1, y1) of the bottom right corner.
+    the coordinates (x1, y1) of the top right corner.
 
     Args:
         x0 (int): The x coordinate of the bottom left corner.
-        x1 (int): The x coordinate of the bottom right corner.
+        x1 (int): The x coordinate of the top right corner.
         y0 (int): The y coordinate of the bottom left corner.
-        y1 (int): The y coordinate of the bottom right corner.
+        y1 (int): The y coordinate of the top right corner.
 
     Attributes:
         x0 (int): The x coordinate of the bottom left corner.
-        x1 (int): The x coordinate of the bottom right corner.
+        x1 (int): The x coordinate of the top right corner.
         y0 (int): The y coordinate of the bottom left corner.
-        y1 (int): The y coordinate of the bottom right corner.
+        y1 (int): The y coordinate of the top right corner.
         width (int): The width of the box, equal to x1 - x0.
         height (int): The height of the box, equal to y1 - y0.
     """
