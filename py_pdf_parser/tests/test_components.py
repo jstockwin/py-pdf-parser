04f6c341c972bb79f4f1732d76bd3f243706c7e1
Jean Garret <jean.garret@billmonitor.com>
2019-12-04 13:55:15 +0000

    [tests] Add tests for PDFDocument

--- py_pdf_parser/tests/test_components.py
+++ py_pdf_parser/tests/test_components.py
@@ -1,13 +1,17 @@
 from ddt import ddt, data
-from unittest import TestCase
 
 from py_pdf_parser.common import BoundingBox
+from py_pdf_parser.components import PDFDocument
+from py_pdf_parser.filtering import ElementList
+from py_pdf_parser.loaders import Page
+from py_pdf_parser.exceptions import NoElementsOnPageError, PageNotFoundError
+from py_pdf_parser.tests.base import BaseTestCase
 
-from utils import create_pdf_element
+from utils import create_pdf_element, FakePDFMinerTextElement
 
 
 @ddt
-class TestPDFElement(TestCase):
+class TestPDFElement(BaseTestCase):
     element_bbox = BoundingBox(2, 5, 2, 5)
 
     def test_index(self):
@@ -134,3 +138,102 @@ class TestPDFElement(TestCase):
     def test_entirely_within_false(self, bounding_box):
         element = create_pdf_element(self.element_bbox)
         self.assertFalse(element.entirely_within(bounding_box))
+
+
+class TestPDFDocument(BaseTestCase):
+    def test_document(self):
+        el_page_1_top_left = FakePDFMinerTextElement(BoundingBox(0, 1, 2, 3))
+        el_page_1_top_right = FakePDFMinerTextElement(BoundingBox(2, 3, 2, 3))
+        el_page_1_bottom_left = FakePDFMinerTextElement(BoundingBox(0, 1, 0, 1))
+        el_page_1_bottom_right = FakePDFMinerTextElement(BoundingBox(2, 3, 0, 1))
+        page_1 = Page(
+            elements=[
+                el_page_1_top_left,
+                el_page_1_top_right,
+                el_page_1_bottom_left,
+                el_page_1_bottom_right,
+            ],
+            width=100,
+            height=100,
+        )
+
+        el_page_2_top_left = FakePDFMinerTextElement(BoundingBox(0, 1, 2, 3))
+        el_page_2_top_right = FakePDFMinerTextElement(BoundingBox(2, 3, 2, 3))
+        el_page_2_bottom_left = FakePDFMinerTextElement(BoundingBox(0, 1, 0, 1))
+        el_page_2_bottom_right = FakePDFMinerTextElement(BoundingBox(2, 3, 0, 1))
+        page_2 = Page(
+            elements=[
+                el_page_2_bottom_right,
+                el_page_2_bottom_left,
+                el_page_2_top_right,
+                el_page_2_top_left,
+            ],
+            width=100,
+            height=100,
+        )
+
+        document = PDFDocument(pages={1: page_1, 2: page_2})
+
+        # Checks elements were reordered
+        expected_ordered_list = [
+            el_page_1_top_left,
+            el_page_1_top_right,
+            el_page_1_bottom_left,
+            el_page_1_bottom_right,
+            el_page_2_top_left,
+            el_page_2_top_right,
+            el_page_2_bottom_left,
+            el_page_2_bottom_right,
+        ]
+        self.assertEqual(
+            [elem.original_element for elem in document.element_list],
+            expected_ordered_list,
+        )
+
+        # Checks indexes were assigned properly
+        self.assertEqual(
+            [elem.index for elem in document.element_list], [0, 1, 2, 3, 4, 5, 6, 7]
+        )
+
+        # Checks number of pages is correct
+        self.assertEqual(document.number_of_pages, 2)
+
+        # Checks pages were assigned properly
+        self.assertEqual(
+            [elem.page_number for elem in document.element_list],
+            [1, 1, 1, 1, 2, 2, 2, 2],
+        )
+
+        # Checks pages were instantiated correctly
+        pdf_page_1 = document.get_page(1)
+        self.assertEqual(page_1.width, pdf_page_1.width)
+        self.assertEqual(page_1.height, pdf_page_1.height)
+        self.assertEqual(el_page_1_top_left, pdf_page_1.start_element.original_element)
+        self.assertEqual(
+            el_page_1_bottom_right, pdf_page_1.end_element.original_element
+        )
+        self.assertEqual(pdf_page_1.page_number, 1)
+        self.assertEqual(pdf_page_1.elements, ElementList(document, set([0, 1, 2, 3])))
+
+        pdf_page_2 = document.get_page(2)
+        self.assertEqual(page_2.width, pdf_page_2.width)
+        self.assertEqual(page_2.height, pdf_page_2.height)
+        self.assertEqual(el_page_2_top_left, pdf_page_2.start_element.original_element)
+        self.assertEqual(
+            el_page_2_bottom_right, pdf_page_2.end_element.original_element
+        )
+        self.assertEqual(pdf_page_2.page_number, 2)
+        self.assertEqual(pdf_page_2.elements, ElementList(document, set([4, 5, 6, 7])))
+
+        self.assertEqual(document.pages, [pdf_page_1, pdf_page_2])
+
+        self.assertEqual(
+            document.elements, ElementList(document, set([0, 1, 2, 3, 4, 5, 6, 7]))
+        )
+
+        with self.assertRaises(PageNotFoundError):
+            document.get_page(3)
+
+    def test_document_with_blank_page(self):
+        with self.assertRaises(NoElementsOnPageError):
+            PDFDocument(pages={1: Page(elements=[], width=100, height=100)})
