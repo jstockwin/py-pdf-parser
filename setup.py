c8f9f2f01fd76ed8953fcdf1d82f8dbc63817519
Jake Stockwin <jstockwin@gmail.com>
2020-05-14 14:49:51 +0100

    [meta] Small tweaks to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from setuptools import setup, find_packages
 
@@ -7,16 +8,23 @@ if sys.version_info < (3, 6):
     print(sys.stderr, "Your Python is {}".format(sys.version))
     sys.exit(1)
 
+
+ROOT_DIR = os.path.dirname(__file__)
+
+
 setup(
     name="py-pdf-parser",
     packages=find_packages(),
-    exclude=["tests.*", "tests"],
+    exclude=["tests.*", "tests", "docs", "docs.*"],
     version="0.0.1",
     url="https://github.com/jstockwin/py-pdf-parser",
     license="BSD",
-    description="Tool for pdf parsing",
+    description="A tool to help extracting information from structured PDFs.",
+    long_description=open(os.path.join(ROOT_DIR, "README.md")).read(),
+    long_description_content_type="text/markdown",
     author="Jake Stockwin",
     author_email="jstockwin@gmail.com",
+    include_package_data=True,
     install_requires=[
         "pdfminer.six==20200402",
         "docopt==0.6.2",
