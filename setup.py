e921763d7c13d7738d2c0623df8476845e3a53bd
Michael Apozyan <Michael.Apozyan@drillinginfo.com>
2024-04-15 18:18:58 +0200

    Update version of pdfminer.six

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     author_email="jstockwin@gmail.com",
     include_package_data=True,
     install_requires=[
-        "pdfminer.six==20220524",
+        "pdfminer.six==20231228",
         "docopt==0.6.2",
         "wand==0.6.10",
     ],
